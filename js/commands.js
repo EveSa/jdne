7f8b5bbc48ef6f9f118e446153eca846e9218437
EveSa <93133249+EveSa@users.noreply.github.com>
2024-12-03 13:30:27 +0100

    Update commands.js
    Signed-off-by: EveSa <93133249+EveSa@users.noreply.github.com>

--- js/commands.js
+++ js/commands.js
@@ -6,27 +6,13 @@ var instagram = "https://www.instagram.com/forrestpknight/";
 var github = "https://github.com/forrestknight/";
 var email = 'mailto:forrest@fkcodes.com';
 
-whois = [
-  "<br>",
-  "Hey, I'm Forrest!👋",
-  "I'm a software engineer and content creator, who builds engaging websites like this one",
-  "and makes YouTube videos about computer science & software engineering.",
-  "After graduating with a Bachelor's in Computer Science, I worked professionally",
-  "as a software engineer building enterprise web applications for Fortune 500 companies.",
-  "While doing all of that, I documented my coding journey on YouTube - trying to enlighten",
-  "the next generation of developers and help them navigate the crazy world that is software", "engineering & computer science.",
-  "Before I knew it, that online presence took on a life of its own, to the point where I knew",
-  "I needed to make the jump from software engineering to full time content creation - strike while",
-  "the iron's hot.",
-  "Now, I make videos about software engineering and computer science, building anything my heart desires,",
-  "like this terminal website.",
-  "<br>"
+ls = [
+  "next.md",
+  "chiffré.md"
 ];
 
-whoami = [
-  "<br>",
-  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
-  "<br>"
+"cat next.md" = [
+  "lieu de vie"
 ];
 
 social = [
